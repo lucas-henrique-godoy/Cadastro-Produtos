ab6cb17e7a907142b7f9a8c4fcc13719502c8158
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-31 21:47:04 -0300

    Update produto.service.ts

--- cadastro-produtos/src/app/services/produto.service.ts
+++ cadastro-produtos/src/app/services/produto.service.ts
@@ -19,4 +19,33 @@ export class ProdutoService {
   }
 
   //Salvar um produto (novo ou atualizar)
+  save(produto: Produto): void {
+    const produtos = this.getAll();
+    const index = produtos.findIndex(p => p.id === produto.id);
+
+    if (index === -1) {
+      //Novo produto - gerar id único
+      produto.id = this.generateId(produtos);
+      produtos.push(produto);
+    } else {
+      //Atuaizar produto existente
+      produtos[index] = produto;
+    }
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(produtos));
+  }
+
+  //deletar produto pelo id
+  delete(id: number): void {
+    let produtos = this.getAll();
+    produtos = produtos.filter(p => p.id !== id);
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(produtos));
+  }
+  
+  //Gerar um id único  incremental
+  private generateId(produtos: Produto[]): number {
+    if (produtos.length === 0) {
+      return 1;
+    }
+    return Math.max(...produtos.map(p => p.id)) + 1;
+  }
 }
