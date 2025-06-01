b92360360d58884fb98c8d1bcbd9f8eb8e7200bb
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-05-31 22:09:01 -0300

    Update produto.service.ts

--- cadastro-produtos/src/app/services/produto.service.ts
+++ cadastro-produtos/src/app/services/produto.service.ts
@@ -20,8 +20,8 @@ export class ProdutoService {
 
   //Salvar um produto (novo ou atualizar)
   save(produto: Produto): void {
-    const produtos = this.getAll();
-    const index = produtos.findIndex(p => p.id === produto.id);
+    const produtos = this.getAll();//Pega todos os produtos cadastrados
+    const index = produtos.findIndex(p => p.id === produto.id); //Busca o indice de um produto que tenha  mesmo id, e findIndex retorna -1 se não encontrar.
 
     if (index === -1) {
       //Novo produto - gerar id único
@@ -37,15 +37,15 @@ export class ProdutoService {
   //deletar produto pelo id
   delete(id: number): void {
     let produtos = this.getAll();
-    produtos = produtos.filter(p => p.id !== id);
-    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(produtos));
+    produtos = produtos.filter(p => p.id !== id); //cria um novo array sem o produto do id informado.
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(produtos)); //Atualizamos o localStorage com o novo array.
   }
   
-  //Gerar um id único  incremental
+  //Gerar um id único  incremental(Esse método calcula o próximo id disponível.)
   private generateId(produtos: Produto[]): number {
     if (produtos.length === 0) {
-      return 1;
+      return 1;                         //Se a lista estiver vazia, retorna 1.
     }
-    return Math.max(...produtos.map(p => p.id)) + 1;
+    return Math.max(...produtos.map(p => p.id)) + 1; //Se houver produtos, pega o maior id e soma 1,Isso evita duplicidade de id, mesmo sem banco de dados.
   }
 }
